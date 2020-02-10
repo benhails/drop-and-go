b5932c8bb8ea71cb8451ca62cc016cb77d01fdf2
Ben Hails <benhails@hotmail.com>
2020-02-10 16:58:41 +0800

    updated requirements.txt and added cities test file

---
+++ models/constants/cities.py
@@ -0,0 +1,7 @@
+CITIES = (
+    {'id': 1, 'city': 'Kuala Lumpur', 'country': 'Malaysia'},
+    {'id': 2, 'city': 'London', 'country': 'United Kingdom'}
+)
+
+
+# c = [city.get('city') + ', ' + city.get('country') for city in CITIES]
