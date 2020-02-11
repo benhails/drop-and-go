6ce7f0411c752b5d5740075566a9fe4e45f9e00e
Syafiq Nazim <syafiqnazim.sns@gmail.com>
2020-02-11 12:07:41 +0800

    Fixed the calling database issue

--- drop_and_go_api/__init__.py
+++ drop_and_go_api/__init__.py
@@ -1,12 +1,12 @@
+from drop_and_go_api.blueprints.stores.views import stores_api_blueprint
+from drop_and_go_api.blueprints.users.views import users_api_blueprint
 from app import app
 from flask_cors import CORS
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 ## API Routes ##
-from drop_and_go_api.blueprints.users.views import users_api_blueprint
-from drop_and_go_api.blueprints.stores.views import stores_api_blueprint
 
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
-app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/stores')
+app.register_blueprint(stores_api_blueprint, url_prefix='/api/v1/stores')
