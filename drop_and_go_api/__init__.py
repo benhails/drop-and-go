1820ce341a3f9d2f8762884a6461ecfe52390983
Ben Hails <benhails@hotmail.com>
2020-02-11 11:06:35 +0800

    added stores model and routes

--- drop_and_go_api/__init__.py
+++ drop_and_go_api/__init__.py
@@ -5,6 +5,8 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 ## API Routes ##
 from drop_and_go_api.blueprints.users.views import users_api_blueprint
+from drop_and_go_api.blueprints.stores.views import stores_api_blueprint
 
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/stores')
